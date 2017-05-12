ce0c8c6eb2fe0a5560c2fd1e211804512647aa7e
Cameron Seid <wincam97@gmail.com>
2017-05-12 08:17:52 -0500

    adjust for consistency

--- doritobot.go
+++ doritobot.go
@@ -290,8 +290,8 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				"thorax":  "Thorax",
 				"shining": "Shiny",
 				"shiny":   "Shiny",
-				"quartz":  "quartz",
-				"dyed":    "quartz",
+				"quartz":  "Quartz",
+				"dyed":    "Quartz",
 			}
 			possibleResponses := []string{
 				"\"ees\" softly.",
@@ -313,7 +313,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				"Delta":  {"gets a wingboner.", "vibrates."},
 				"Jac":    {"dies of cuteness overload.", "passes out from an extreme overdose of gay.", "can't hold all these husbandos."},
 				"Twisty": {"invites you to his next gig.", "needed that! :heart:"},
-				"quartz": {"runs away.", "did not like that"},
+				"Quartz": {"runs away.", "did not like that."},
 			}
 			if names[msg[1]] == "" {
 				s.ChannelMessageSend(m.ChannelID, "I'm afraid I don't know who that is. :c")
