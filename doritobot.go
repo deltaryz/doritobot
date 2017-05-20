c3cdf3ca4eab5d4c66b5762330e7a3648f75841d
Nazo-baka <nazo@floof.zone>
2017-05-19 21:02:11 -0500

    Add Woona

--- doritobot.go
+++ doritobot.go
@@ -314,7 +314,8 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				"ice":     "Ice Bear",
 				"bear":    "Ice Bear",
 				"carson":  "Dragon",
-				"dragon":  "Dragon",
+				"dragon":  "Dragon",	
+				"woona":   "Woona",
 			}
 			possibleResponses := []string{
 				"snuggles back.",
@@ -339,6 +340,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				"Rhombus":  {"giggles like a giddy schoolfilly.", "squeals happily.", "floofs his wings."},
 				"Ice Bear": {"doesn't hate your butt.", "has a conspiracy theory.", "has respect. Keep real.", "...sleeps in... fridge...", "will lick your cheeks."},
 				"Dragon":   {"licks you sensually.", "closes his jaw around your head.", "picks you up and holds you like a toy.", "thinks you have a pretty mane.", "rubs his claws through your mane."},
+				"Woona":    {"gives you her Cartographer's Cap.", "squees happily! <:customemote:315306330384629760>"},
 			}
 			if names[msg[1]] == "" {
 				s.ChannelMessageSend(m.ChannelID, "I'm afraid I don't know who that is. :c")
